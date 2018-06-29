cbf13569b8248672e259a8297474882421426a78
Satyam <satyam7991@gmail.com>
2018-06-29 18:01:13 +0530

    register company completed

--- src/pages/companyForm/companyForm.ts
+++ src/pages/companyForm/companyForm.ts
@@ -1,16 +1,88 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { ToastController } from "ionic-angular";
+import { NavController, NavParams } from "ionic-angular";
+import { Http } from "@angular/http";
+import "rxjs/add/operator/map";
 
 @Component({
-  templateUrl: 'companyFOrm.html'
+	templateUrl: "companyFOrm.html"
 })
 export default class CompanyForm {
+	constructor(
+		public navCtrl: NavController,
+		public navParams: NavParams,
+		public http: Http,
+		private toastCtrl: ToastController
+	) {}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	title = this.navParams.data.title;
+	header = this.navParams.data.header;
+	editCompany = this.navParams.data.company;
 
-  }
+	presentToast(message) {
+		let toast = this.toastCtrl.create({
+			message: message,
+			duration: 3000,
+			position: "bottom"
+		});
 
-  title = this.navParams.data.title;
-  header = this.navParams.data.header;
-  company = this.navParams.data.company;
+		toast.onDidDismiss(() => {
+			console.log("Dismissed toast");
+		});
+
+		toast.present();
+	}
+
+	company = this.title==='Register Company'?{
+		name: null,
+		profile: null,
+		openings: null
+	}:this.editCompany;
+
+	onNameChange(e: any) {
+		this.company.name = e.target.value;
+	}
+
+	onProfileChange(e: any) {
+		this.company.profile = e.target.value;
+	}
+
+	onOpeningsChange(e: any) {
+		this.company.openings = e.target.value;
+	}
+
+	handleSave() {
+		this.company.openings += "";
+		if (this.title === "Register Company") {
+			if (this.validate()) {
+				this.http.post('http://localhost:8080/registerCompany',this.company).subscribe(data => console.log(data));
+				this.presentToast(`A new company ${this.company.name} is registered.`);
+				this.navCtrl.pop();
+			}
+		} else {
+			// if(this.validate()) {
+			// 	this.http.put(`http://localhost:8080/editStudent/${this.editStudent._id}`, this.student).subscribe(data => console.log(data));
+			// 	this.presentToast(`A student named ${this.student.name} is updated.`);
+			// 	this.navCtrl.pop();
+			// }
+		}
+	}
+
+	validate() {
+		if (this.company.name === null) {
+			this.presentToast('Name cannot be empty');
+			return false;
+		} else if (this.company.profile === null) {
+			this.presentToast('Profile cannot be empty');
+			return false;
+		} else if (this.company.openings === null) {
+			this.presentToast('Openings No cannot be empty');
+			return false;
+		} else if(isNaN(this.company.openings) || this.company.openings<1){
+			this.presentToast('Invalid Openings');
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
