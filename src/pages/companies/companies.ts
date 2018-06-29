d0cb54cf355738dd861498520a0d80c905256adf
Satyam <satyam7991@gmail.com>
2018-06-29 17:23:16 +0530

    all companies fetched

--- src/pages/companies/companies.ts
+++ src/pages/companies/companies.ts
@@ -2,37 +2,33 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import CompanyForm from '../companyForm/companyForm';
 import CompanyDetail from '../companyDetail/companyDetail';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   templateUrl: 'companies.html'
 })
 export default class CompaniesList {
 
-  constructor(public navCtrl: NavController) {
+  companies: any = null;
 
+  constructor(public navCtrl: NavController, public http: Http) {
+    this.fetchCompanies();
   }
 
-  items = [
-    {
-      name: 'c1',
-      profile: 'sde',
-      openings: 2
-    },
-    {
-      name: 'c2',
-      profile: 'hr',
-      openings: 5
-    },
-    {
-      name: 'c3',
-      profile: 'analyst',
-      openings: 4
-    }
-  ];
+  fetchCompanies() {
+    this.http.get('http://localhost:8080/companies').map(res => res.json()).subscribe(data => {
+      this.companies = data;
+    });
+  }
+  
+  ionViewWillEnter() {
+    this.fetchCompanies();
+  }
 
-  itemSelected(item: any) {
+  companySelected(company: any) {
     this.navCtrl.push(CompanyDetail,{
-      item: item
+      item: this.companies
     })
   }
 
