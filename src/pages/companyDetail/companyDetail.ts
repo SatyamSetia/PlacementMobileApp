7c2e35e9b072624b2718c218b11c9ca6937ebe67
Satyam <satyam7991@gmail.com>
2018-06-29 18:57:20 +0530

    unregister company

--- src/pages/companyDetail/companyDetail.ts
+++ src/pages/companyDetail/companyDetail.ts
@@ -1,23 +1,50 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import CompanyForm from '../companyForm/companyForm';
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+import CompanyForm from "../companyForm/companyForm";
+import { Http } from "@angular/http";
+import "rxjs/add/operator/map";
+import { ToastController } from "ionic-angular";
 
 @Component({
-  templateUrl: 'companyDetail.html'
+  templateUrl: "companyDetail.html"
 })
-
 export default class CompanyDetail {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public http: Http,
+    private toastCtrl: ToastController
+  ) {}
 
   company = this.navParams.data.company;
 
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: "bottom"
+    });
+
+    toast.onDidDismiss(() => {
+      console.log("Dismissed toast");
+    });
+
+    toast.present();
+  }
+
   openCompanyForm(company: any) {
-    this.navCtrl.push(CompanyForm,{
-      title: 'Edit',
-      header: 'Update Details',
+    this.navCtrl.push(CompanyForm, {
+      title: "Edit",
+      header: "Update Details",
       company: company
     });
+  }
+
+  deleteCompany() {
+    this.http
+      .delete(`http://localhost:8080/unregisterCompany/${this.company._id}`)
+      .subscribe(data => console.log(data));
+    this.presentToast(`A company named ${this.company.name} is deleted.`);
+    this.navCtrl.pop();
   }
 }
